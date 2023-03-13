7b57a636042eab7afe0b834f51e4717c669e0111
Luka <47296785+lukadev-0@users.noreply.github.com>
2023-03-13 17:36:55 +0100

    feat: :fire: remove analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,5 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: vercel({
-    analytics: true,
-  }),
+  adapter: vercel(),
 });
