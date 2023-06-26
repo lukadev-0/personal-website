53293c4b3b2a0145f726e450f1fa64632dba04cd
Luka <47296785+lukadev-0@users.noreply.github.com>
2023-06-26 17:52:39 +0200

    chore: switch to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import vercel from "@astrojs/vercel/static";
-
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  adapter: vercel(),
+  site: 'https://lukadev.me/',
 });
