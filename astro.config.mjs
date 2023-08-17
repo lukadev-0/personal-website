7ebe2661415970a6d0012415a7030834a66c75d9
LukaDev <47296785+lukadev-0@users.noreply.github.com>
2023-08-17 17:17:08 +0200

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind(), mdx(), sitemap()],
   site: 'https://lukadev.me/'
 });
