0db1c8613ef4441d1d375dca36f5b838050a3bfd
LukaDev <47296785+lukadev-0@users.noreply.github.com>
2024-11-22 19:58:29 +0100

    chore: destroy everything

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,9 @@ import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
-import react from "@astrojs/react";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://lukadev.me",
-  integrations: [mdx(), tailwind(), sitemap(), react()]
+  integrations: [mdx(), tailwind(), sitemap()],
 });
+
